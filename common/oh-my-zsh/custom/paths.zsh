eb07e57a849c18a0f46e1514f0079c2437b7a317
Sreekanth S <s.sreekanth@outlook.com>
2016-07-19 19:20:11 -0700

    zsh : set PATH to os-specific paths

--- common/oh-my-zsh/custom/paths.zsh
+++ common/oh-my-zsh/custom/paths.zsh
@@ -1,1 +1,7 @@
-export PATH="$HOME/.bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/games:/usr/games:/snap/bin"
+if is_darwin; then
+    export PATH="$HOME/.bin:$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/X11/bin"
+elif is_linux; then
+    export PATH="$HOME/.bin:$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/games:/usr/games:/snap/bin"
+elif is_windows; then
+    export PATH="$HOME/.bin:$HOME/bin:/usr/local/bin:$PATH"
+fi
