8c32d72d343cd0a7d502fff64cd8a252d8823885
Sreekanth S <s.sreekanth@outlook.com>
2016-08-31 17:41:59 -0700

    zsh: add GOROOT-based install location to PATH. Also, add go and vagrant to brew packages list on macOS

--- common/oh-my-zsh/custom/custom/paths.zsh
+++ common/oh-my-zsh/custom/custom/paths.zsh
@@ -1,5 +1,6 @@
 if is_darwin; then
     export PATH="$HOME/.bin:$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/X11/bin"
+    export PATH=$PATH:/usr/local/opt/go/libexec/bin
 elif is_linux; then
     export PATH="$HOME/.bin:$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/games:/usr/games:/snap/bin"
 elif is_windows; then
