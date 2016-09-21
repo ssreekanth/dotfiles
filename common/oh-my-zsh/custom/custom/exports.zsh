b1749b5b5cb0d09af14c16d5b9bf553dac544379
Sreekanth S <s.sreekanth@outlook.com>
2016-09-20 21:59:43 -0700

    set EDITOR environment variable to either nvim or vim or vi in that order

--- common/oh-my-zsh/custom/custom/exports.zsh
+++ common/oh-my-zsh/custom/custom/exports.zsh
@@ -1,2 +1,10 @@
+if binary_exists nvim; then
+    export EDITOR=nvim
+elif binary_exists vim ; then
+    export EDITOR=vim
+elif binary_exists vi ; then
+    export EDITOR=vi
+fi
+
 export VIM_PLUGIN_TMP_DIR=$HOME/.tmp/vim/plugged
 export TMUX_PLUGIN_TMP_DIR=$HOME/.tmp/tmux/plugins
