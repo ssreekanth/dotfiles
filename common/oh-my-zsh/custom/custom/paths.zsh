83dcc48fdac29211e779e1f14ff67ec6f03c0e41
Sreekanth S <s.sreekanth@outlook.com>
2021-01-17 10:15:44 -0800

    add Linux Homebrew to PATH

--- common/oh-my-zsh/custom/custom/paths.zsh
+++ common/oh-my-zsh/custom/custom/paths.zsh
@@ -1,11 +1,11 @@
 if is_darwin; then
-    export PATH=$PATH:"$HOME/.bin:$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/X11/bin"
-    export PATH=$PATH:/usr/local/opt/go/libexec/bin
+    export PATH="$PATH:$HOME/.bin:$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/X11/bin"
+    export PATH="$PATH:/usr/local/opt/go/libexec/bin"
     export PATH="/usr/local/opt/node@6/bin:$PATH"
 elif is_linux; then
-    export PATH=$PATH:"$HOME/.bin:$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/games:/usr/games:/snap/bin"
+    export PATH="$PATH:$HOME/.bin:$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/games:/usr/games:/snap/bin"
 elif is_windows; then
-    export PATH=$PATH:"$HOME/.bin:$HOME/bin:/usr/local/bin:$PATH"
+    export PATH="$PATH:$HOME/.bin:$HOME/bin:/usr/local/bin:$PATH"
 fi
 
 export PATH=$PATH:${GOBIN}
